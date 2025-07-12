3d21bcb557748da7bb84f822d6a82eacaea870e9
Shwetanshu1 <shwetanshujha60@gmail.com>
2025-07-12 23:18:22 +0530

    Enhanced login and submit pages UI, improved layout and styling

---
+++ script.js
@@ -0,0 +1,11 @@
+function goHome() {
+  window.location.href = 'index.html';
+}
+
+function goLogin() {
+  window.location.href = 'login.html';
+}
+
+function goSubmit() {
+  window.location.href = 'submit.html';
+}
